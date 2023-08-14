11ff836a756edfc85b0f0d1ecc4f7b3cfa4fbb89
Ricci <60737357+Jogaridu@users.noreply.github.com>
2023-08-14 08:56:25 -0300

    Update conexao_db.py

--- src/database/conexao_db.py
+++ src/database/conexao_db.py
@@ -1,14 +1,17 @@
 import pymongo
 
+uri = "localhost"
+uri = "mongodb+srv://dtsld:ka7HwyA7bjsBSJki@cluster0.lr1zeas.mongodb.net/?retryWrites=true&w=majority"
+
 def instanciar_processos():
-    client = pymongo.MongoClient("localhost", 27017)
+    client = pymongo.MongoClient(uri, 27017)
     db = client["datashield_com_br"]
     colecao = db["processos"]
     return colecao
 
 
 def instanciar_analise():
-    client = pymongo.MongoClient("localhost", 27017)
+    client = pymongo.MongoClient(uri, 27017)
     db = client["datashield_com_br"]
     colecao = db["analise"]
     return colecao
