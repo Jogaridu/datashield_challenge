3aa41565429ab5509871d083b61eace6d0102588
Jorge Gabriel <e-jorge2010@hotmail.com>
2023-08-25 08:18:08 -0300

    fix: instancia de objeto

--- src/models/monitorar_processos.py
+++ src/models/monitorar_processos.py
@@ -244,33 +244,5 @@ def validarResultados(pid, resultados):
     return retorno
 
 
-#iniciar()
-
-# CÓDIGO ABAIXO É PARA AVALIAR RESULTADOS DO MONGODB
-colecao_processos.find()
-documentos = list(colecao_processos.find())
-
-resultado = {}
-
-atributos_analise = ['handleCount', 'pageFaults', 'pageFileUsage', 'peakPageFileUsage', 'workingSetSize', 'threadCount', 'priority', 'privatePageCount']
-
-for atributo in atributos_analise:
-    resultado[atributo] = {
-        'atributo': '',
-        'maior_valor': float('-inf')
-    }
-
-for atributo in atributos_analise:
-    maior_valor = float('-inf')
-    processo_nome = ''
-    for json_obj in documentos:
-        if atributo in json_obj['dadosProcesso']:
-            valor = json_obj['dadosProcesso'][atributo]
-            if float(valor) > float(maior_valor):
-                maior_valor = valor
-                processo_nome = json_obj['nomeProcesso']
-                resultado[atributo]['atributo'] = processo_nome
-                resultado[atributo]['maior_valor'] = maior_valor
-
-print(resultado)
+monitoramento = Monitoramento()
  
